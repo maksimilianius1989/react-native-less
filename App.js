7c407d40790bc22554e2e78fd26f2a1683977046
Bukach_M <maksym.bukach@volia.com>
2019-09-19 14:40:45 +0300

    Less 1

--- App.js
+++ App.js
@@ -26,8 +26,8 @@ import {
 
 const App = () => {
   return (
-    <View>
-      <Text>
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
         Restaurant Review
       </Text>
     </View>
@@ -35,42 +35,18 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+        color: 'red',
+    },
 });
 
 export default App;
