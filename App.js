f8d1fbf7a96e7c5232ccf9f708ceb0b54c6139f8
Vimax <it.vimax@gmail.com>
2019-09-21 21:49:44 +0300

    Less 5 :: Import style file

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {
-  View,
-  Text,
+    View,
+    Text,
+    StyleSheet
 } from 'react-native';
 
+import HeaderStyle from './HeaderStyle'
+
 const restaurants = [
-    {name: "React Cafe", address: "123 Anywhere St"},
+    {name: "React Cafe", address: "222 Anywhere St"},
     {name: "Fancy Restaurant", address: "799 Main St"},
     {name: "Taco Place", address: "550 Maple Rd"}
 ]
@@ -15,43 +18,26 @@ const App = () => {
     <View style={{
         flex: 1
     }}>
-        <Text style={{
-          padding: 40,
-          fontSize: 30,
-          textAlign: 'center',
-          color: '#0066CC',
-          fontWeight: '300'
-        }}>
-            Restaurant Review
-        </Text>
-
+        <Text style={HeaderStyle.header}>Restaurant Review</Text>
         {
             restaurants.map((place, index) => {
                 return (
-                    <View key={place.name} style={{
-                        flexDirection: 'row'
-                    }}>
-                        <View style={{
-                            flex: 1,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }}>
+                    <View key={place.name} style={[
+                        styles.row,
+                        {
+                            backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7'
+                        }
+                    ]}>
+                        <View style={styles.edges}>
                             <Text>{index + 1}</Text>
                         </View>
 
-                        <Text style={{
-                            flexDirection: 'column',
-                            flex: 8
-                        }}>
+                        <Text style={styles.nameAddress}>
                             <Text>{place.name}</Text>
-                            <Text style={{color: 'grey'}}>{place.address}</Text>
+                            <Text style={styles.addressText}>{place.address}</Text>
                         </Text>
 
-                        <View style={{
-                            flex: 1,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }}>
+                        <View style={styles.edges}>
                             <Text>Info</Text>
                         </View>
                     </View>
@@ -63,3 +49,22 @@ const App = () => {
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row'
+    },
+    edges: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 5
+    },
+    nameAddress: {
+        flexDirection: 'column',
+        flex: 8
+    },
+    addressText: {
+        color: 'grey'
+    }
+})
