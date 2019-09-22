e85bac98cfe38edcef1c5c8616c68ef127ee88e2
Vimax <it.vimax@gmail.com>
2019-09-22 09:20:09 +0300

    Change App from function to class

--- App.js
+++ App.js
@@ -1,52 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TextInput
 } from 'react-native';
 
 import HeaderStyle from './HeaderStyle'
 
 const restaurants = [
-    {name: "React Cafe", address: "222 Anywhere St"},
+    {name: "React Cafe", address: "111 Anywhere St"},
     {name: "Fancy Restaurant", address: "799 Main St"},
     {name: "Taco Place", address: "550 Maple Rd"}
 ]
 
-const App = () => {
-  return (
-    <View style={{
-        flex: 1
-    }}>
-        <Text style={HeaderStyle.header}>Restaurant Review</Text>
-        {
-            restaurants.map((place, index) => {
-                return (
-                    <View key={place.name} style={[
-                        styles.row,
-                        {
-                            backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7'
-                        }
-                    ]}>
-                        <View style={styles.edges}>
-                            <Text>{index + 1}</Text>
-                        </View>
+class App extends Component {
 
-                        <Text style={styles.nameAddress}>
-                            <Text>{place.name}</Text>
-                            <Text style={styles.addressText}>{place.address}</Text>
-                        </Text>
+    render() {
+        return (
+            <View style={{
+                flex: 1
+            }}>
+                <Text style={HeaderStyle.header}>Restaurant Review</Text>
 
-                        <View style={styles.edges}>
-                            <Text>Info</Text>
-                        </View>
-                    </View>
-                )
-            })
-        }
-    </View>
-  );
-};
+                <TextInput
+                    style={styles.input}
+                    placeholder="Live Search"
+                    onChangeText={text => {
+                        this.setState({search: text})
+                    }}
+                />
+
+                {
+                    restaurants.map((place, index) => {
+                        return (
+                            <View key={place.name} style={[
+                                styles.row,
+                                {
+                                    backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7'
+                                }
+                            ]}>
+                                <View style={styles.edges}>
+                                    <Text>{index + 1}</Text>
+                                </View>
+
+                                <Text style={styles.nameAddress}>
+                                    <Text>{place.name}</Text>
+                                    <Text style={styles.addressText}>{place.address}</Text>
+                                </Text>
+
+                                <View style={styles.edges}>
+                                    <Text>Info</Text>
+                                </View>
+                            </View>
+                        )
+                    })
+                }
+            </View>
+        )
+    }
+}
 
 export default App;
 
@@ -66,5 +79,15 @@ const styles = StyleSheet.create({
     },
     addressText: {
         color: 'grey'
+    },
+    input: {
+        marginBottom: 30,
+        padding: 10,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
+        backgroundColor: '#F5F5F5'
     }
 })
