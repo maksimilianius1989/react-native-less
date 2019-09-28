d3a27129503f9c83464467364ef4df3d92c76e79
Vimax <it.vimax@gmail.com>
2019-09-28 13:49:55 +0300

    Lessons 22 :: Added KeyboardAwareScrollView, but this dont working

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -3,18 +3,32 @@ import {
     View,
     Text,
     TouchableOpacity,
-    StyleSheet
+    StyleSheet,
+    TextInput
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view"
 
 export default class AddReview extends Component {
+    state = {
+        name: '',
+        rating: 0,
+        comment: ''
+    }
+
     close = () => {
         this.props.navigation.goBack()
     }
 
     render () {
         return (
-            <View style={styles.root}>
+            <KeyboardAwareScrollView
+                style={{
+                    flex: 1,
+                    backgroundColor: '#FFF'
+                }}
+            >
+
                 <TouchableOpacity
                     style={styles.button}
                     onPress={this.close}
@@ -24,7 +38,48 @@ export default class AddReview extends Component {
 
                 <Text style={styles.addReview}>Add Review</Text>
 
-            </View>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Name (optional)"
+                    value={this.state.name}
+                    onChangeText={name => this.setState({name})}
+                />
+
+                <Text style={styles.rating}>Your Rating:</Text>
+                <View style={styles.stars}>
+                    {
+                        [1, 2, 3, 4, 5].map(i => {
+                            return (
+                                <TouchableOpacity
+                                    onPress={() => this.setState({rating: i})}
+                                    style={styles.starButton}
+                                    key={i}
+                                >
+                                    <Icon
+                                        name={"star"}
+                                        color={this.state.rating >= i ? "#FFD64C" : "#CCC"}
+                                        size={40}
+                                    />
+                                </TouchableOpacity>
+                            )
+                        })
+                    }
+                </View>
+
+                <TextInput
+                    style={[styles.input, {height: 100}]}
+                    placeholder="Review"
+                    value={this.state.review}
+                    onChangeText={review => this.setState({review})}
+                    multiline={true}
+                    numberOfLines={5}
+                />
+
+                <TouchableOpacity style={styles.submitButton}>
+                    <Text style={styles.submitButtonText}>Submit Review</Text>
+                </TouchableOpacity>
+
+            </KeyboardAwareScrollView>
         )
     }
 }
@@ -43,5 +98,40 @@ const styles = StyleSheet.create({
         color: '#444',
         textAlign: 'center',
         margin: 20
+    },
+    input: {
+        padding: 10,
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 3
+    },
+    rating: {
+        fontSize: 20,
+        color: 'grey',
+        textAlign: 'center',
+        marginVertical: 40
+    },
+    stars: {
+        marginBottom: 80,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    starButton: {
+        padding: 5
+    },
+    submitButton: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#0066cc',
+        borderRadius: 4,
+        marginVertical: 10,
+        marginHorizontal: 20
+    },
+    submitButtonText: {
+        fontSize: 18,
+        color: '#fff',
+        textAlign: 'center'
     }
 })
